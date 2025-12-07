3decfdd22427d20739287fb81953f4fa997d38b0
Leonardoup10 <leonardo.101296@hotmail.com>
2025-12-07 12:16:49 +0000

    debug: hardcode api url

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
+    baseURL: 'https://unhappy-carmine-waterlog.ngrok-free.dev', // Hardcoded for debugging
     headers: {
         'ngrok-skip-browser-warning': 'true',
         'Content-Type': 'application/json'
